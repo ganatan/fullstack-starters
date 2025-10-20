4bdb104b1e3b275cc4244c2447c27411126b377e
ganatan <dannyganatan@gmail.com>
2025-10-20 08:00:57 +0200

    remove useless comment

--- angular/06-angular-lazy-loading/src/app/app.routes.ts
+++ angular/06-angular-lazy-loading/src/app/app.routes.ts
@@ -5,14 +5,3 @@ export const routes: Routes = [
   { path: 'about', loadComponent: () => import('./pages/about/about').then(m => m.About) },
   { path: '**', redirectTo: '' }
 ];
-
-
-// import { Routes } from '@angular/router';
-// import { Home } from './pages/home/home';
-// import { About } from './pages/about/about';
-
-// export const routes: Routes = [
-//   { path: '', component: Home },
-//   { path: 'about', component: About },
-//   { path: '**', redirectTo: '' }
-// ];
