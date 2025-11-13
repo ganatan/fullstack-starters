178b9b9216f21c4273198092db89ed1bdddab485
ganatan <dannyganatan@gmail.com>
2025-11-13 08:41:58 +0100

    Improve typescript-starters

--- typescript/01-typescript-callback/src/app.ts
+++ typescript/01-typescript-callback/src/app.ts
@@ -1,20 +1,38 @@
-function add() {
-  console.log('00000000001:add');
-  let result = 'ADD';
+function information(): string {
+  console.log('00000000001:information');
+  let result = 'information';
   return result;
 }
 
-function del() {
-  console.log('00000000001:del');
-  let result = 'DEL';
+function boxoffice(): string {
+  console.log('00000000001:boxoffice');
+  let result = 'boxoffice';
   return result;
 }
 
-function getShows(toto: string, momo: () => any) {
+function getShows(param: string, callback: () => string) {
   console.log('00000000001:getShows');
-  console.log('00000000001:getShows:' + toto);
-  momo();
+  console.log('00000000002:getShows:' + param);
+  let result = callback();
+  console.log('00000000003:getShows:' + result);
 }
 
-getShows('Aliens',add)
-getShows('Exodus',del)
+getShows('Aliens', information);
+getShows('Exodus', boxoffice);
+
+
+
+function informationMedia(name: string): string {
+  console.log('00000000001:information');
+  let result = 'information on : ' + name;
+  return result;
+}
+
+function getMedias(param: string, callback: (mediaName: string) => string) {
+  console.log('00000000001:getMedias');
+  console.log('00000000002:getMedias:' + param);
+  let result = callback(param);
+  console.log('00000000003:getMedias:' + result);
+}
+
+getMedias('Exodus', information);
