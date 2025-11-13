98b8d9aa2521094aea17030be7409dc61a1daa42
ganatan <dannyganatan@gmail.com>
2025-11-13 16:41:23 +0100

    Add angular-signals

---
+++ angular/31-angular-signals/src/app/app.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular-starter');
+
+  movies: String[] = [
+    'Aliens',
+    'Gladiator',
+    'Exodus',
+    'Legend',
+  ];
+
+}
